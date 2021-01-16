6684479214bff06ea8a4775a4d4b7843bd787823
Ermolenko.K <19symbols@gmail.com>
2021-01-16 16:14:31 +0300

    Настроен вью контроллер для работы с камерой

--- SwiftUI-Barcode-Scanner/BarcodeScannerView.swift
+++ SwiftUI-Barcode-Scanner/BarcodeScannerView.swift
@@ -8,31 +8,31 @@
 import SwiftUI
 
 struct BarcodeScannerView: View {
-  var body: some View {
-    NavigationView {
-      VStack {
-        Rectangle().frame(maxWidth: .infinity, maxHeight: 300)
+    var body: some View {
+        NavigationView {
+            VStack {
+                Rectangle().frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer().frame(height: 60)
+                
+                Label("Scanned Barcode", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text("Not scanned yet")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+            }
+            .navigationTitle(Text("Barcode Scanner"))
+        }
         
-        Spacer().frame(height: 60)
-        
-        Label("Scanned Barcode", systemImage: "barcode.viewfinder")
-          .font(.title)
-        
-        Text("Not scanned yet")
-          .bold()
-          .font(.largeTitle)
-          .foregroundColor(.green)
-          .padding()
-      }
-      .navigationTitle(Text("Barcode Scanner"))
     }
-    
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    BarcodeScannerView()
-      .preferredColorScheme(.light)
-  }
+    static var previews: some View {
+        BarcodeScannerView()
+            .preferredColorScheme(.light)
+    }
 }
