23125ce263dbc346e27958f1555071dd874e4baf
Ermolenko.K <19symbols@gmail.com>
2021-01-16 14:36:39 +0300

    Initial Commit

---
+++ SwiftUI-Barcode-Scanner/SwiftUI_Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Barcode_ScannerApp.swift
+//  SwiftUI-Barcode-Scanner
+//
+//  Created by Ермоленко Константин on 16.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
