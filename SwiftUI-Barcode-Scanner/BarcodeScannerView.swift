9830f36e6ec9e20a48e30e60be341c4f7bcd94b0
Ermolenko.K <19symbols@gmail.com>
2021-01-16 19:48:57 +0300

    Отображение ошибок в виде стандартного Предупреждения (Alert)

--- SwiftUI-Barcode-Scanner/BarcodeScannerView.swift
+++ SwiftUI-Barcode-Scanner/BarcodeScannerView.swift
@@ -7,14 +7,32 @@
 
 import SwiftUI
 
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: String
+    let message: String
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input",
+                                              message: "Something went wrong with the camera. We are unable to capture the input.",
+                                              dismissButton: .default(Text("Ok")))
+    static let invalidScannedValue = AlertItem(title: "invalid Scanned Value",
+                                              message: "The value scanned is not valid. This app scans EAN-8 and EAN-13.",
+                                              dismissButton: .default(Text("Ok")))
+}
+
 struct BarcodeScannerView: View {
     
-    @State var scannedCode = ""
+    @State private var scannedCode = ""
+    @State private var alertItem: AlertItem?
     
     var body: some View {
         NavigationView {
             VStack {
-                ScannerView(scannedCode: $scannedCode)
+                ScannerView(scannedCode: $scannedCode,
+                            alertItem: $alertItem)
                     .frame(maxWidth: .infinity, maxHeight: 300)
                 
                 Spacer().frame(height: 60)
@@ -29,6 +47,11 @@ struct BarcodeScannerView: View {
                     .padding()
             }
             .navigationTitle(Text("Barcode Scanner"))
+            .alert(item: $alertItem) { alertItem in
+                Alert(title: Text(alertItem.title),
+                      message: Text(alertItem.message),
+                      dismissButton: alertItem.dismissButton)
+            }
         }
         
     }
