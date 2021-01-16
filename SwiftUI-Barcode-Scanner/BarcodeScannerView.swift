b2bbcba3489167eec2632f3bbb6f0e56f3478bd2
Ermolenko.K <19symbols@gmail.com>
2021-01-16 14:55:19 +0300

    Настроен экран приложения. Настройка разрешения доступа к камере (Privacy settings)

---
+++ SwiftUI-Barcode-Scanner/BarcodeScannerView.swift
@@ -0,0 +1,38 @@
+//
+//  BarcodeScannerView.swift
+//  SwiftUI-Barcode-Scanner
+//
+//  Created by Ермоленко Константин on 16.01.2021.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+  var body: some View {
+    NavigationView {
+      VStack {
+        Rectangle().frame(maxWidth: .infinity, maxHeight: 300)
+        
+        Spacer().frame(height: 60)
+        
+        Label("Scanned Barcode", systemImage: "barcode.viewfinder")
+          .font(.title)
+        
+        Text("Not scanned yet")
+          .bold()
+          .font(.largeTitle)
+          .foregroundColor(.green)
+          .padding()
+      }
+      .navigationTitle(Text("Barcode Scanner"))
+    }
+    
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    BarcodeScannerView()
+      .preferredColorScheme(.light)
+  }
+}
